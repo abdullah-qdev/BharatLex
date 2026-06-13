d2c51310ee645ff2e37c96427329e69bc64f8a48
sofian229 <146734513+sofian229@users.noreply.github.com>
2026-06-14 00:25:35 +0530

    Deleted backed

--- backend/services/mongodb.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import os
-
-client = MongoClient(os.getenv("MONGODB_URI"))
-db = client["bharatlex"]
-
-legal_docs = db["legal_docs"]
-users = db["users"]
-complaints = db["complaints"]
